e7809cca0dbc01423565f10ea84c803da57d74c1
Mathialagan <mathialagan823@gmail.com>
2025-08-15 12:36:15 +0530

    add the files

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/Portfolio/", // 👈 Required for GitHub Pages
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
