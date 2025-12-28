350a71c2aad7114a1f5dc5130edd15d6ad35c6a0
Mathialagan23 <mathialagan823@gmail.com>
2025-12-28 12:13:18 +0530

    Netlify ready

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
-  base: "/Portfolio/", // 👈 Required for GitHub Pages
+  // 👇 base changes depending on where you deploy
+  base: mode === "production" ? "/" : "/",
   plugins: [
     react(),
     mode === "development" && componentTagger(),
@@ -20,3 +20,29 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   base: "/Portfolio/", // 👈 Required for GitHub Pages
+//   plugins: [
+//     react(),
+//     mode === "development" && componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
